cdcfeeede6cb5f0238a77c2ad4761c43c5210ec3
turfguy <ksm0811@ajou.ac.kr>
2022-11-18 22:27:31 +0900

    kakakmap error fix / styled-components

--- components/postForm.js
+++ components/postForm.js
@@ -2,7 +2,9 @@ import {Button, Form, Input,Container} from 'antd'
 import { useCallback, useRef, useState, useEffect} from 'react';
 import Script from 'next/script';
 import { Map, MapMarker } from "react-kakao-maps-sdk";
-	
+import Head from 'next/head';
+import styled from 'styled-components';
+
 const PostForm = () =>
 {   
     
@@ -19,16 +21,19 @@ const PostForm = () =>
     
     return(
         <>
-                <Script
+        <Head>
+            <script
                 type="text/javascript"
                 src="//dapi.kakao.com/v2/maps/sdk.js?appkey=cde9d5084f1eaf59090943b96589e58f&libraries=services,clusterer&autoload=false"        
                 
-                />
+                />   
+        </Head>
+                
            
         <Form style={{margin: '10px 0 20px', marginTop: '20px'}} encType="multipart/form-data" onFinish={onSubmit}>
               <Map
                     center={{ lat: 33.5563, lng: 126.79581 }}
-                    style={{ width: "100%", height: "200px" }}
+                    style={{ width: "100%", height: "360px" }}
                     >
                     <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
                
